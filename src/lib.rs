58a17bd459fe4cb35e5651e6076d416a54dcea43
Clinton Selke <clinuxrulz@gmail.com>
2021-06-28 16:07:54 +1000

    added cloned! macro to assist with shared signals/memos

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod macros;
 mod reactivity;
 
 pub use reactivity::{batch, Effect, Memo, Signal};
@@ -6,12 +7,7 @@ pub use reactivity::{batch, Effect, Memo, Signal};
 fn test_effect() {
     let mut a = Signal::new(1);
     let mut b = Signal::new(2);
-    let c;
-    {
-        let a = a.clone();
-        let b = b.clone();
-        c = Memo::new(move || *a.read() + *b.read());
-    }
+    let c = Memo::new(cloned!((a, b) => move || *a.read() + *b.read()));
     let _effect = Effect::new(move || println!("{}", *c.read()));
     *a.write() = 3;
     batch(|| {
@@ -23,11 +19,7 @@ fn test_effect() {
 #[test]
 fn test_calmed() {
     let mut a: Signal<i32> = Signal::new(1);
-    let b;
-    {
-        let a = a.clone();
-        b = Memo::new_calmed_eq(move || *a.read());
-    }
+    let b = Memo::new(cloned!((a) => move || *a.read()));
     let _effect = Effect::new(move || println!("{}", *b.read()));
     *a.write() = 2;
     *a.write() = 2;
---
+++ src/macros.rs
@@ -0,0 +1,8 @@
+#[macro_export]
+macro_rules! cloned {
+    (($($arg:ident),*) => $e:expr) => {{
+        // clone all the args
+        $( let $arg = ::std::clone::Clone::clone(&$arg); )*
+        $e
+    }};
+}
